bf198a21d20af196561a1d05ac033fd54966dfcb
figraham <fi.sage.graham@gmail.com>
2019-04-10 16:32:46 -0600

    TerminalDOM started

--- src/_jasmine/terminal/TerminalDOM.spec.ts
+++ src/_jasmine/terminal/TerminalDOM.spec.ts
@@ -1,5 +1,79 @@
 import { TerminalDOM } from '../../terminal/TerminalDOM';
+import 'jasmine-dom-custom-matchers';
 
 describe('TerminalDOM Units: ', () => {
-  // TODO
+
+  beforeAll(() => {
+    // @ts-ignore
+    jasmine.addMatchers(DOMCustomMatchers);
+  })
+  
+  let dom
+
+  beforeEach(() => {
+    let div = document.createElement('div');
+    document.body.appendChild(div);
+    dom = new TerminalDOM(div);
+  });
+
+  it('DOM creation unit', () => {
+    expect(dom.container).toBeHTMLElement();
+    expect(dom.pre).toBeHTMLElement();
+    expect(dom.code).toBeHTMLElement();
+    expect(dom.display).toBeHTMLElement();
+  });
+  
+  it('DOM structure unit', () => {
+    expect(dom.container).toBeChildOf(document.body);
+    expect(dom.pre).toBeChildOf(dom.container);
+    expect(dom.code).toBeChildOf(dom.pre);
+    expect(dom.display).toBeChildOf(dom.code);
+  });
+
+  it('DOM number of children unit', () => {
+    expect(dom.container).toHaveChildren(1);
+    expect(dom.pre).toHaveChildren(1);
+    expect(dom.code).toHaveChildren(1);
+  });
+
+  it('DOM addInnerTxt to display', () => {
+    let helloString = 'Hello TerminalTXT!';
+    dom.addInnerTxt(helloString);
+    expect(dom.display.innerHTML).toEqual(helloString);
+    let goodbyeString = 'Goodbye';
+    dom.addInnerTxt(goodbyeString);
+    expect(dom.display.innerHTML).toEqual(helloString + goodbyeString);
+    dom.addInnerMultiTxt([helloString, goodbyeString]);
+    expect(dom.display.innerHTML).toEqual(helloString + goodbyeString + helloString + goodbyeString);
+  });
+
+  it('DOM setInnerTxt to display', () => {
+    let helloString = 'Hello TerminalTXT!';
+    dom.setInnerTxt(helloString);
+    expect(dom.display.innerHTML).toEqual(helloString);
+    let goodbyeString = 'Goodbye';
+    dom.setInnerTxt(goodbyeString);
+    expect(dom.display.innerHTML).toEqual(goodbyeString);
+    dom.setInnerMultiTxt([helloString, goodbyeString]);
+    expect(dom.display.innerHTML).toEqual(helloString + goodbyeString);
+  });
+
+  it('DOM addChildElement to display', () => {
+    let hansel = document.createElement('div');
+    dom.addChildElement(hansel);
+    expect(dom.display.children.length).toEqual(1);
+    let gretel = document.createElement('div');
+    dom.addChildElement(gretel);
+    expect(dom.display.children.length).toEqual(2);
+  });
+
+  it('DOM setChildElement to display', () => {
+    let hansel = document.createElement('div');
+    dom.setChildElement(hansel);
+    expect(dom.display.children.length).toEqual(1);
+    let gretel = document.createElement('div');
+    dom.setChildElement(gretel);
+    expect(dom.display.children.length).toEqual(1);
+  });
+
 });
--- src/terminal/TerminalDOM.ts
+++ src/terminal/TerminalDOM.ts
@@ -3,4 +3,132 @@
  */ // TODO
 export class TerminalDOM {
 
+  /**
+   * The main DOM wrapper for all DOM elements.
+   * Nothing from the terminal should be outside of this element.
+   */
+  public container: HTMLDivElement;
+
+  /**
+   * Preformatted Text, should be the direct child of the container.
+   */
+  public pre: HTMLPreElement;
+  /**
+   * Code / Monospace, should be the direct child of the pre tag.
+   */
+  public code: HTMLElement;
+
+  /**
+   * Span that contains the final display text of the terminal.
+   */
+  public display: HTMLSpanElement;
+
+  /**
+   * @param container
+   */
+  constructor(container: HTMLDivElement) {
+    this.container = container;
+    this.pre = document.createElement('pre');
+    this.code = document.createElement('code');
+    this.display = document.createElement('span');
+
+    this.container.appendChild(this.pre);
+    this.pre.appendChild(this.code);
+    this.code.appendChild(this.display);
+  }
+
+  /**
+   * Adds text to innerHTML of display.
+   * 
+   * @param txt 
+   */
+  protected addInnerTxt(txt: string): void {
+    const txtArray: string[] = [];
+    txtArray.push(txt);
+    this.addInnerMultiTxt(txtArray);
+  }
+
+  /**
+   * Adds multiple strings of text to innerHTML of display.
+   * 
+   * @param txtArray 
+   */
+  protected addInnerMultiTxt(txtArray: string[]): void {
+    for (let i: number = 0; i < txtArray.length; i++) {
+      this.display.innerHTML += (txtArray[i]);
+    }
+  }
+
+  /**
+   * Clears and then sets the innerHTML of display to text.
+   * 
+   * @param txt 
+   */
+  protected setInnerTxt(txt: string): void {
+    const txtArray: string[] = [];
+    txtArray.push(txt);
+    this.setInnerMultiTxt(txtArray);
+  }
+
+  /**
+   * Clears and then sets the innerHTML of display to multiple strings of text.
+   * 
+   * @param txtArray 
+   */
+  protected setInnerMultiTxt(txtArray: string[]): void {
+    this.clearInnerTxt();
+    this.addInnerMultiTxt(txtArray);
+  }
+
+  /**
+   * Clears all text from innerHTML of display.
+   */
+  protected clearInnerTxt(): void {
+    this.display.innerHTML = '';
+  }
+
+  /**
+   * Appends child element to display.
+   * 
+   * @param element 
+   */
+  protected addChildElement(element: HTMLElement): void {
+    const elements: HTMLElement[] = [];
+    elements.push(element);
+    this.addChildElements(elements);
+  }
+
+  /**
+   * Appends multiple children to display.
+   * 
+   * @param elements 
+   */
+  protected addChildElements(elements: HTMLElement[]): void {
+    for (let i: number = 0; i < elements.length; i++) {
+      this.display.appendChild(elements[i]);
+    }
+  }
+
+  /**
+   * Clears all children elements from display and then appends a new child on.
+   * 
+   * @param element 
+   */
+  protected setChildElement(element: HTMLElement): void {
+    const elements: HTMLElement[] = [];
+    elements.push(element);
+    this.setChildElements(elements);
+  }
+
+  protected setChildElements(elements: HTMLElement[]): void {
+    this.clearChildElements();
+    this.addChildElements(elements);
+  }
+
+  protected clearChildElements(): void {
+    while(this.display.firstChild) {
+      this.display.removeChild(this.display.firstChild);
+    }
+  }
+
 }
