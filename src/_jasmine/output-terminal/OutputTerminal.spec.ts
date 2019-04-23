50c98db7a17d77a561f61da5450175a1c03c6129
figraham <fi.sage.graham@gmail.com>
2019-04-23 00:08:04 -0600

    basic OutputTerminal

--- src/_jasmine/output-terminal/OutputTerminal.spec.ts
+++ src/_jasmine/output-terminal/OutputTerminal.spec.ts
@@ -1,5 +1,67 @@
 import { OutputTerminal } from '../../output-terminal/OutputTerminal';
+import { TerminalConfig } from '../../config/TerminalConfig';
 
 describe('CommandTerminal Units: ', () => {
-  // TODO
+
+  let term: OutputTerminal;
+  
+  it('default constructor unit', () => {
+    term = new OutputTerminal();
+    expect(term.getWidth()).toEqual(80);
+    expect(term.getHeight()).toEqual(25);
+  });
+
+  it('param constructor unit', () => {
+    const randomDiv: HTMLDivElement = document.createElement('div');
+    term = new OutputTerminal({
+      width: 10,
+      height: 10,
+      container: randomDiv,
+    } as TerminalConfig, 'Hello!');
+    expect(term.getWidth()).toEqual(10);
+    expect(term.getHeight()).toEqual(10);
+    // @ts-ignore
+    expect(term.lineController.lines[term.lineController.lines.length -1].innerHTML).toEqual('Hello!');
+  });
+
+  it('param constructor unit', () => {
+    term = new OutputTerminal({width: 1} as TerminalConfig, 'This is too long!!!!!!');
+    // @ts-ignore
+    expect(term.lineController.lines[term.lineController.lines.length -1].innerHTML).toEqual('');
+  });
+
+  it('writeln unit', () => {
+    term = new OutputTerminal();
+
+    let randoms: string[] = [];
+    for (let i = 0; i < 100; i++) {
+      randoms.push(Math.random().toString());
+    }
+
+    for (let i = 0; i < randoms.length; i++) {
+      term.writeln(randoms[i]);
+      // @ts-ignore
+      expect(term.lineController.lines[term.lineController.lines.length -1].innerHTML).toEqual(randoms[i]);
+    }
+  });
+  
+  it('writeln too long unit', () => {
+    term = new OutputTerminal();
+
+    let randoms: string[] = [];
+    for (let i = 0; i < 100; i++) {
+      let s: string = '';
+      for (let j = 0; j < 10; j++) {
+        s += Math.random().toString();
+      }
+      randoms.push(s);
+    }
+
+    for (let i = 0; i < randoms.length; i++) {
+      term.writeln(randoms[i]);
+      // @ts-ignore
+      expect(term.lineController.lines[term.lineController.lines.length -1].innerHTML).toEqual(randoms[i].substring((Math.floor(randoms[i].length / 80) * 80)));
+    }
+  });
+
 });
--- src/output-terminal/OutputTerminal.ts
+++ src/output-terminal/OutputTerminal.ts
@@ -3,22 +3,74 @@ import { DOMLineController } from '../dom-controller/DOMLineController';
 
 /**
  * Terminal for taking in Text input commands from user.
+ * 
+ * Notes:
+ * - If config.height is passed -1 no cropping will happen.
  */
 export class OutputTerminal {
 
+  /**
+   * Height of OutputTerminal.
+   */
   protected height: number;
 
+  /**
+   * [[DOMLineControl]] for this OutputTerminal.
+   */
   protected lineController: DOMLineController;
 
+  /**
+   * Width of OutputTerminal
+   */
   protected width: number;
 
-  constructor(config: TerminalConfig = {} as TerminalConfig) {
+  /**
+   * @param config 
+   * @param initialText The first line displayed, won't work if larger than width.
+   */
+  constructor(config: TerminalConfig = {} as TerminalConfig, initialText: string = '') {
     config.width ? this.width = config.width : this.width = 80;
     config.height ? this.height = config.height : this.height = 25;
     if (config.container) {
       this.lineController = new DOMLineController(this.height, config.container);
     } else {
       this.lineController = new DOMLineController(this.height);
+    }
+    if (initialText.length > this.width) {
+      initialText = ''
+    }
+    this.lineController.setCurrentLine(initialText);
+  }
+
+  /**
+   * @returns [[height]]
+   */
+  public getHeight(): number {
+    return this.height;
+  }
+
+  /**
+   * @returns [[width]]
+   */
+  public getWidth(): number {
+    return this.width;
+  }
+
+  /**
+   * Will write a line to the OutputTerminal.
+   * 
+   * @param text 
+   */
+  public writeln(text: string): void {
+    if (text.length <= this.width) {
+      this.lineController.addLine(text);
+    } else {
+      const chunks: RegExpMatchArray | null = text.match(new RegExp('.{1,' + this.width + '}', 'g'));
+      if (chunks !== null) {
+        for (let i: number = 0; i < chunks.length; i++) {
+          this.lineController.addLine(chunks[i]);
+        }
+      }
     }
   }
 
